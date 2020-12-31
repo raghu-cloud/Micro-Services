409a4edb126dcb7bd19fa9e06932a87f154363bc
Raghu <raghupatil19@gmail.com>
2021-01-01 00:39:37 +0530

    Level-3_Micro-Service-configuration

--- spring-boot-config/src/main/java/com/javabrains/springbootconfig/resource/Controller.java
+++ spring-boot-config/src/main/java/com/javabrains/springbootconfig/resource/Controller.java
@@ -2,6 +2,7 @@ package com.javabrains.springbootconfig.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +14,26 @@ public class Controller {
     @Value("${greeting.message}")
     public String greetings;
 
+    @Value("${app.name}")
+    public String appName;
+
     @Value("${numbers.list}")
     private List<String> listOfNumber;
-
-    @Value("#{${dbValues}}")
-    private Map<String,String> dbValues;
 
     @Autowired
     private DbValues dataBaseValues;
 
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/greeting")
     public String greet(){
-        return dataBaseValues.getConnection()+" "+dataBaseValues.getHost();
+        return dataBaseValues.getConnection();
     }
+
+    @RequestMapping("/getEnvDetails")
+    public String envDetails(){
+        return env.toString();
+    }
+
 }
